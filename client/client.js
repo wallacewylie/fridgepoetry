9a2e6675e6e9637ecf170bdc5e3b7e500861cfca
wallacewylie <wallace.wylie@gmail.com>
2016-07-14 21:18:48 -0500

    Event Handler Test

---
+++ client/client.js
@@ -0,0 +1,16 @@
+document.getElementById('ball2').onmousedown = function() {
+    this.style.position = 'absolute';
+    var self = this;
+    document.onmousemove = function(e) {
+        e = e || event;
+        fixPageXY(e);
+        self.style.left = e.pageX-25+'px';
+        self.style.top = e.pageY-25+'px';
+    };
+    this.onmouseup = function() {
+        document.onmousemove = null;
+    }
+}
+
+document.getElementById('ball2').ondragstart = function() { return false };
+
