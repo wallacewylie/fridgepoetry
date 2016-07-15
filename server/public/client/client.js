4d617275ceaa1f87a3955e645ca2fce21a7ae656
wallacewylie <wallace.wylie@gmail.com>
2016-07-15 16:42:56 -0500

    New Dragging Element

--- server/public/client/client.js
+++ server/public/client/client.js
@@ -1,16 +1,36 @@
-document.getElementById('ball2').onmousedown = function() {
-    this.style.position = 'absolute';
-    var self = this;
-    document.onmousemove = function(e) {
-        e = e || event;
-        fixPageXY(e);
-        self.style.left = e.pageX-25+'px';
-        self.style.top = e.pageY-25+'px';
-    };
-    this.onmouseup = function() {
-        document.onmousemove = null;
+var selected = null, // Object of the element to be moved
+    x_pos = 0, y_pos = 0, // Stores x & y coordinates of the mouse pointer
+    x_elem = 0, y_elem = 0; // Stores top, left values (edge) of the element
+
+// Will be called when user starts dragging an element
+function _drag_init(elem) {
+    // Store the object of the element which needs to be moved
+    selected = elem;
+    x_elem = x_pos - selected.offsetLeft;
+    y_elem = y_pos - selected.offsetTop;
+}
+
+// Will be called when user dragging an element
+function _move_elem(e) {
+    x_pos = document.all ? window.event.clientX : e.pageX;
+    y_pos = document.all ? window.event.clientY : e.pageY;
+    if (selected !== null) {
+        selected.style.left = (x_pos - x_elem) + 'px';
+        selected.style.top = (y_pos - y_elem) + 'px';
     }
+}
+
+// Destroy the object when we are done
+function _destroy() {
+    selected = null;
+}
+
+// Bind the functions...
+document.getElementById('draggable-element').onmousedown = function () {
+    _drag_init(this);
+    return false;
 };
 
-document.getElementById('ball2').ondragstart = function() { return false };
+document.onmousemove = _move_elem;
+document.onmouseup = _destroy;
 
